a04f97802791991047dbe59770fdb184fa0a8f5a
Maciej <Maciej@DESKTOP-5NJT1J0>
2025-02-16 14:44:00 +0100

    Dodaj pliki projektów.

---
+++ 02_pascal_triangle/02_pascal_triangle.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+std::vector<int> get_next_row(std::vector<int>& last_row) {
+    std::vector<int> next_row{ 1 };
+    if (last_row.empty()) {
+        return next_row;
+    }
+    else {
+        for (size_t i = 0; i + 1 < last_row.size(); ++i) {
+            next_row.emplace_back(last_row[i] + last_row[i + 1]);
+        }
+        next_row.emplace_back(1);
+		return next_row;
+    }
+}
+
+auto generate_triangle(int rows) {
+    std::vector<std::vector<int>> triangle{ 1 };
+    for (int i = 1; i < rows; i++) {
+        triangle.push_back(get_next_row(triangle.back()));
+    }
+    return triangle;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
