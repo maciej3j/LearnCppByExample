054fa87a333bc07b041e2fb35d03f8e36841360d
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-02-18 16:41:28 +0100

    .

--- 03_guessing_game/main.cpp
+++ 03_guessing_game/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <limits>
+#include <random>
+#include <optional>
+#include <functional>
+#include <format>
+#include <concepts>
 
-unsigned int some_const_number(){
-    return 42;
+auto some_random_number(){
+    std::random_device rd;
+    std::mt19937 engine(rd());
+    std::uniform_int_distribution<int>dist(1, 100);
+    return dist(engine);
 }
 
 unsigned input() {
@@ -15,16 +23,51 @@ unsigned input() {
     return number;
 }
 
-void guess_number(unsigned number) {
-    std::cout << "Guess the number.\n";
-    unsigned guess = input();
-    while (guess != number) {
-        std::cout << guess << " is wrong. Try again\n";
-        guess = input();
+template <typename T, typename P>
+class Map{
+public:
+    T m_key;
+    P m_value;
+    Map(T new_key, P new_value)
+        : m_key{new_key}, m_value{new_value} {}
+};
+
+
+std::function<std::string(int, int)> callable = [](int number, int guess) {
+    return std::format("Your guess was too {}\n", (guess < number ? "small" : "big"));
+};
+
+std::optional<int> read_number(std::istream& in) {
+    int result{};
+    if (in >> result) {
+        return result;
     }
-    std::cout << "Well done.\n";
+    in.clear();
+    in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    return {};
+}
+
+void guess_number_with_clues(unsigned number, std::invocable<int, int> auto message) {
+    std::cout << "Guess the number.\n";
+    std::optional<int> guess;
+    while ((guess = read_number(std::cin))) {
+        if (guess.value() == number) {
+            std::cout << "Well done.\n";
+            return;
+        }
+        std::cout << message(number, guess.value());
+        std::cout << '>';
+    }
+    std::cout << "The number was " << number << "\n";
+
 }
 
 int main() {
-    guess_number(some_const_number());
+    // auto make_message = [](int number, int guess) {
+    //     return std::format("Your guess was too {}\n",  (guess < number ? "small" : "big"));
+    // };
+    // guess_number_with_clues(some_random_number(), make_message);
+    Map map1(111222,std::string{"Steve Murphy"});
+    std::cout << "key : " << map1.m_key << std::endl;
+    std::cout << "value : " << map1.m_value << std::endl;
 }
